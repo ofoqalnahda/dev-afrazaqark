8a061092f2c9c14556fc62df8f8faa7b8a74a9a1
acer <A3malalnahda@gmail.com>
2024-12-15 13:36:38 +0300

    Set password for user

--- Modules/Auth/App/Models/User.php
+++ Modules/Auth/App/Models/User.php
@@ -26,7 +26,7 @@ class User extends Authenticatable implements JWTSubject, HasMedia
         'email_verified_at' => 'datetime',
     ];
 
-    public function setPasswordAttribute($value)
+    public function setPasswordAttribute($value): string
     {
         return $this->attributes['password'] = bcrypt($value);
 
